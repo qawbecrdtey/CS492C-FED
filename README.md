e8ecd3c92c7b7e37d7fa28bfe4d4779aa83f86a0
esproject-pico <54151812+esproject-pico@users.noreply.github.com>
2021-10-30 16:14:48 +0900

    markdown test page

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # CS492C-FED
 커뮤니케이션 도구 게시판/협업 도구 개발
+
+설치
+node.js
+react
+express
+storybook
+
+
+npm i react-mde
+https://github.com/andrerpena/react-mde
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,92 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from "react";
+import ReactMde from "react-mde";
+import ReactDOM from "react-dom";
+import * as Showdown from "showdown";
+import "./styles.css";
+import "react-mde/lib/styles/css/react-mde-all.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function loadSuggestions(text) {
+  return new Promise((accept, reject) => {
+    setTimeout(() => {
+      const suggestions = [
+        {
+          preview: "Andre",
+          value: "@andre"
+        },
+        {
+          preview: "Angela",
+          value: "@angela"
+        },
+        {
+          preview: "David",
+          value: "@david"
+        },
+        {
+          preview: "Louise",
+          value: "@louise"
+        }
+      ].filter((i) => i.preview.toLowerCase().includes(text.toLowerCase()));
+      accept(suggestions);
+    }, 250);
+  });
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const converter = new Showdown.Converter({
+  tables: true,
+  simplifiedAutoLink: true,
+  strikethrough: true,
+  tasklists: true
+});
+
+export default function App() {
+  const [value, setValue] = React.useState("**Hello world!!!**");
+  const [selectedTab, setSelectedTab] = React.useState("write");
+
+  const save = async function* (data) {
+    // Promise that waits for "time" milliseconds
+    const wait = function (time) {
+      return new Promise((a, r) => {
+        setTimeout(() => a(), time);
+      });
+    };
+
+    // Upload "data" to your server
+    // Use XMLHttpRequest.send to send a FormData object containing
+    // "data"
+    // Check this question: https://stackoverflow.com/questions/18055422/how-to-receive-php-image-data-over-copy-n-paste-javascript-with-xmlhttprequest
+
+    await wait(2000);
+    // yields the URL that should be inserted in the markdown
+    yield "https://picsum.photos/300";
+    await wait(2000);
+
+    // returns true meaning that the save was successful
+    return true;
+  };
+
+  return (
+    <div className="container">
+      <ReactMde
+        value={value}
+        onChange={setValue}
+        selectedTab={selectedTab}
+        onTabChange={setSelectedTab}
+        generateMarkdownPreview={(markdown) =>
+          Promise.resolve(converter.makeHtml(markdown))
+        }
+        loadSuggestions={loadSuggestions}
+        childProps={{
+          writeButton: {
+            tabIndex: -1
+          }
+        }}
+        paste={{
+          saveImage: save
+        }}
+      />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
