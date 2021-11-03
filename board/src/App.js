dca00a7899d66b03e0422cefe562c0276ba610a8
amy279 <amy279@kaist.ac.kr>
2021-11-03 16:12:34 +0900

    remake..

--- board/src/App.js
+++ board/src/App.js
@@ -1,44 +1,18 @@
 import { BrowserRouter, Route } from 'react-router-dom';
-import React, {Component, useEffect} from 'react';
 import PostMain from './page/post/PostMain';
 import PostView from './page/post/PostView';
-import axios from 'axios'
+import Landing from './page/post/Landing';
  
-const App = () => {
-  useEffect ( () => {
-    axios.get("/api/hello").then(res => console.log(res.data))
-  }, []);
-
+function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Route exact path='/postView/:no' component={PostView} />
-        <Route exact path='/' component={PostMain} />
+        <Route exact path='/postMain/' component={PostMain} />
+        <Route exact path='/' component={Landing} />
       </BrowserRouter>
     </div>
   );
 }
-
-// class App extends Component {
-//   state = {
-//     name: null
-//   }
-//   // componentDidMount(){
-//   //   fetch('http://localhost:4000/data1.json')
-//   //   .then(res => res.json())
-//   //   .then(data => this.setState({title : data.name}))
-//   // }
-//   render(){
-//     return(
-//       <div className="App">
-//         {/* <div>{this.state.name}</div> */}
-//         <BrowserRouter>
-//           <Route exact path='/postView/:no' component={PostView} />
-//           <Route exact path='/' component={PostMain} />
-//         </BrowserRouter>
-//       </div>
-//     );
-//   }
-// }
  
 export default App;
