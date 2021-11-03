59658b74bbfd7fb633eca72ae9f75997a2f7c5ec
amy279 <amy279@kaist.ac.kr>
2021-11-03 19:28:17 +0900

    네아로 구현

--- board/server/app.js
+++ board/server/app.js
@@ -1,9 +1,13 @@
 var express = require('express');
-const fs = require('fs');
 // express 는 함수이므로, 반환값을 변수에 저장한다.
 var app = express()
 // 웹브라우저에서 보낸 데이터를 받아서 처리하는 body-parser를 불러온다.
 var bodyParser = require('body-parser')
+var client_id = '_xriyns1iwvtRUz9k3eE';
+var client_secret = 'Z6ce5YPyai';
+var state = "RANDOM_STATE";
+var redirectURI = encodeURI("http://localhost:4000/api/callback");
+var api_url = "";
 
 // 브라우저에서 오는 응답이 json 일수도 있고, 아닐 수도 있으므로 urlencoded() 도 추가한다.
 app.use(bodyParser.json())
@@ -25,35 +29,41 @@ app.listen(4000, function() {
     console.log("start! express server on port 4000")
 })
 
-// app.get("/data1.json", (req,res,next) => {
-//     res.json(data1);
-// })
-
-// //url routing 처리
-// app.get('/', function(req,res) {
-//     res.sendFile(__dirname + "/public/main.html") //__dirname은 요청하고자 하는 파일의 경로를 단축시켜주는 절대경로 식별자.
-// })
-// //
-
-// app.get('/main', function(req,res) {
-//     res.sendFile(__dirname + "/public/main.html")
-// })
-
-// app.get('/form', function(req, res){
-//     res.sendFile(__dirname + "/public/form.html")
-// })
-
-// // app.post('/email_post', function(req, res){
-// //     res.send("<h1> welcome! </h>" + req.body.email)
-// // })
-
-// app.post('/email_post', function(req, res){
-//     res.render('email.ejs', {'email' : req.body.email})
-// })
 
 app.get('/api/hello', (req, res)=> {
-    res.send("안녕");
+    res.send("서버연결성공");
 });
+
+app.get('/api/naverlogin', function (req, res) {
+    api_url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirectURI + '&state=' + state;
+    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+    res.end("<a href='"+ api_url + "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>");
+});
+
+app.get('/api/callback', function (req, res) {
+    // eslint-disable-next-line no-undef
+    code = req.query.code;
+    state = req.query.state;
+    api_url = 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='
+    // eslint-disable-next-line no-undef
+    + client_id + '&client_secret=' + client_secret + '&redirect_uri=' + redirectURI + '&code=' + code + '&state=' + state;
+    var request = require('request');
+    var options = {
+        url: api_url,
+        headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
+    };
+    request.get(options, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+        res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+        res.end(body);
+    } else {
+        res.status(response.statusCode).end();
+        console.log('error = ' + response.statusCode);
+    }
+    });
+});
+
+
 
 
 // public 디렉토리를 static으로 기억한다.
