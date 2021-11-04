4bfddc513933d24c93231bb1487c45f9d6493700
amy279 <amy279@kaist.ac.kr>
2021-11-04 20:54:19 +0900

    merge dain&yumin

--- board/src/App.js
+++ board/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import PostMain from './page/post/PostMain';
 import PostView from './page/post/PostView';
 import Landing from './page/post/Landing';
+import Post from './page/post/Post/Post'
  
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       <BrowserRouter>
         <Route exact path='/postView/:no' component={PostView} />
         <Route exact path='/postMain/' component={PostMain} />
-        <Route exact path='/' component={Landing} />
+        {/* <Route exact path='/' component={Landing} /> */}
+        <Route exact path='/' component={Post} />
+        {/* <Route exact path='/Logined/' component={Logined} /> */}
       </BrowserRouter>
     </div>
   );
--- board/src/page/post/Landing.js
+++ board/src/page/post/Landing.js
@@ -3,77 +3,22 @@ import axios from 'axios';
 
 const Landing = () => {//Functional Component 만들기
 
-    const [data, setData] = useState('')
+    // const [data, setData] = useState('')
+    const [api_url, setData] = useState('')
 
     useEffect(() => {
-        axios.get('api/hello')      //endpoint. getRequest를 server 즉 index.js로 보내질 것
-        // .then(response => console.log(response.data))   //server 에서 돌아온 response를 콘솔창에 출력해봄
+        axios.get('api/naverlogin')      //endpoint. getRequest를 server 즉 index.js로 보내질 것
+        // axios.get('api/hello')      //endpoint. getRequest를 server 즉 index.js로 보내질 것
         .then(response => setData(response.data))   //server 에서 돌아온 response를 콘솔창에 출력해봄
     }, [])
     
     
     return(
-         <div>
-          {data}
-         </div>
+        //  <div>
+        //   {data}
+        //  </div>
+        <a href =  {api_url}  ><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>
      )
  }
-
-// const Landing = () => {
-//     const { naver } = window;
- 
-//     const Login = () => {
-//      Naver();
-//      UserProfile();
-//     }
-    
-//     useEffect(Login, []);
-    
-//     const Naver = () => {
-//        const naverLogin = new naver.LoginWithNaverId({
-//          clientId: "_xriyns1iwvtRUz9k3eE",
-//          callbackUrl: "http://localhost:4000/api/login",
-//          isPopup: false,
-//          loginButton: {color: "green", type: 1, height: 30} ,
-//          callbackHandle: true
-//        });
-//        naverLogin.init();
-//      }
-   
-//      const UserProfile = () => {
-//        window.location.href.includes('access_token') && GetUser();
-//        function GetUser() {
-//          const location = window.location.href.split('=')[1];
-//          const token = location.split('&')[0];
-//          console.log("token: ", token);
-//          fetch(`${API}/account/sign-in` , {
-//            method: "GET",
-//            headers : {
-//              "Content-type" : "application/json",
-//              "Authorization": token
-//            },
-//          })
-//          .then(res => res.json())
-//          .then(res => {
-//            localStorage.setItem("access_token", res.token);
-//            setUserData({
-//              nickname : res.nickname,
-//              image : res.image
-//            })
-//          })
-//          .catch(err => console.log("err : ", err));
-//        }
-//      };
-     
-//      return (
-//        <SideLogin className="login">
-//          <UserInfo>
-//            <SideText>로그인</SideText>  
-//          </UserInfo>
-//          <LoginLink onClick={Login} id="naverIdLogin" /> 
-//        </SideLogin>
-//      )
-// };
-
 
  export default Landing
