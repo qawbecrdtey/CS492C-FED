0f94d9fb98cf8069cd16bb9eb624b655ebe38dcc
qawbecrdtey <qawbecrdtey@naver.com>
2021-11-11 16:42:18 +0900

    Modified board: Apply these to the main branch.

--- react-mongodb-pagination/src/Board.js
+++ react-mongodb-pagination/src/Board.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 const constPostPerPage = 15;
-const constPagePerPagination = 11;
+const constPagePerPagination = 10;
 const constTotalPostCount = 345;
 
 /*
@@ -21,14 +21,25 @@ function getPagePerPagination() { return constPagePerPagination; }
 function getTotalPostCount() { return constTotalPostCount; }
 
 function showBoardPage(state) {
-    //const [pageState, setPageState] = useState(state.inBoardPage.page);
-    const previousButton = (pe, pi, tp) => {
-
+    const prevButton = (pe, pi, tp, page) => {
+      if(page <= pi) {
+        return (<button disabled="disabled">prev</button>);
+      }
+      let modifiedState = state;
+      modifiedState.inBoardPage.page--;
+      return (<button onClick={() => showBoardPage(modifiedState)}>prev</button>);
     };
-    const pageNavigator = (pe, pi, tp) => {
-
+    const pageNavigator = (pe, pi, tp, page) => {
     };
-    const nextButton = (pe, pi, tp) => {
+    const nextButton = (pe, pi, tp, page) => {
+      const lastPage = Math.floor((tp - 1) / pe) + 1;
+      const paginationCount = Math.floor((lastPage - 1) / pi) + 1;
+      if(page >= (paginationCount - 1) * pi) {
+        return (<button disabled="disabled">next</button>);
+      }
+      let modifiedState = state;
+      modifiedState.inBoardPage++;
+      return (<button onClick={() => showBoardPage(modifiedState)}>next</button>);
 
     };
 
@@ -41,9 +52,9 @@ function showBoardPage(state) {
             <h1>Board page</h1>
             <div className="UserInfoSpace"></div>
             <div className="Pagination">
-                { previousButton(postPerPage, pagePerPagination, totalPostCount) }
-                { pageNavigator(postPerPage, pagePerPagination, totalPostCount) }
-                { nextButton(postPerPage, pagePerPagination, totalPostCount) }
+                { prevButton(postPerPage, pagePerPagination, totalPostCount, state.inBoardPage.page) }
+                { pageNavigator(postPerPage, pagePerPagination, totalPostCount, state.inBoardPage.page) }
+                { nextButton(postPerPage, pagePerPagination, totalPostCount, state.inBoardPage.page) }
             </div>
         </div>
     )
