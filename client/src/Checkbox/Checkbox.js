2c709f6ed35848d4dbf5e4cde45d657221ebe488
qawbecrdtey <qawbecrdtey@naver.com>
2021-11-21 16:42:01 +0900

    Working on checkbox utilities.

---
+++ client/src/Checkbox/Checkbox.js
@@ -0,0 +1,29 @@
+import { useState, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './Checkbox.css';
+
+const Checkbox = ({ uuid }) => {
+    const [checked, setChecked] = useState(false);
+
+    const checkedItemHandler = (uuid, isChecked) => {
+        if(isChecked) {
+            checkedItems.add(uuid);
+            setCheckedItems(checkedItems);
+        }
+        else if(checkedItems.has(uuid)) {
+            checkedItems.delete(uuid);
+            setCheckedItems(checkedItems);
+        }
+    };
+
+    const checkHandler = ({ e }) => {
+        setChecked(!checked);
+        checkedItemHandler(uuid, e.checked);
+    };
+
+    return (
+        <input type="checkbox" checked={ checked } onChange={ (e) => checkHandler(e) } />
+    );
+};
+
+export default Checkbox;
