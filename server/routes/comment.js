32ef788d585fe93c779fc0cce6fedeb9049a043e
amy279 <amy279@kaist.ac.kr>
2021-11-22 23:45:39 +0900

    like done

--- server/routes/comment.js
+++ server/routes/comment.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable no-shadow */
 /* eslint-disable object-shorthand */
 /* eslint-disable camelcase */
 /* eslint-disable prefer-template */
@@ -14,34 +16,27 @@ const express = require('express');
 const router = express.Router();
 const { Comment } = require('../models/Comment');
 
+router.post('/saveComment', (req, res) => {
+  const comment = new Comment(req.body);
 
-router.post("/saveComment", (req, res) => {
-
-    const comment = new Comment(req.body)
-
-    console.log(comment)
-    comment.save((err, comment) => {
-        if (err) return res.json({ success: false, err })
-        
-        Comment.find({ '_id': comment._id })
-            .populate('writer')
-            .exec((err, result) => {
-                if (err) return res.json({ success: false, err })
-                return res.status(200).json({ success: true, result })
-            })
-    })
-
+  console.log(comment);
+  comment.save((err, comment) => {
+    if (err) return res.json({ success: false, err });
+    Comment.find({ _id: comment._id })
+      .populate('writer')
+      .exec((err, result) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({ success: true, result });
+      });
+  });
 });
 
-router.post("/getComments", (req, res) => {
-
-    Comment.find({ "postNo": req.body.postNo })
-        .populate('writer')
-        .exec((err, comments) => {
-            if (err) return res.status(400).send(err)
-            res.status(200).json({ success: true, comments })
-        })
-
+router.post('/getComments', (req, res) => {
+  Comment.find({ postNo: req.body.postNo })
+    .populate('writer')
+    .exec((err, comments) => {
+      if (err) return res.status(400).send(err);
+      res.status(200).json({ success: true, comments });
+    });
 });
-
 module.exports = router;
