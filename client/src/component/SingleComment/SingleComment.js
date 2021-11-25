887446b0cb22a94a7a2ba2e4edb18fbbcc47d615
yuminyumin <jungym887@naver.com>
2021-11-25 15:39:35 +0900

    add profile card if hover

--- client/src/component/SingleComment/SingleComment.js
+++ client/src/component/SingleComment/SingleComment.js
@@ -1,27 +1,36 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
-import profileImage from '../../images/logo.svg';
-import { BodyContainer, SingleCommentContainer, WriterContainer, ContentContainer } from './styled';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { BodyContainer, SingleCommentContainer, WriterContainer,
+     ContentContainer, HoverContainer, HoverContent, ProfileContent
+    } from './styled';
 
 function SingleComment(props) {
-    const [isAuthorHover, setIsAuthorHover] = useState(false);
-    
+    const test = useSelector(state => state.user);
+    console.log(test)
     return (
         <SingleCommentContainer>
             <BodyContainer>
-                <WriterContainer 
-                    onMouseOver={() => setIsAuthorHover(true)}
-                    onMouseOut={() => setIsAuthorHover(false)}
-                >
-                    <img
-                        src={isAuthorHover ? profileImage : ""}
-                        alt=""
-                    />
-                    {props.comment.writer}
+                <WriterContainer>
+                    <HoverContainer>
+                        {props.comment.writer}
+                        <HoverContent className="profilecard">
+                            <ProfileContent>
+                                작성한 게시물 수: {test.myPostList.length}개
+                            </ProfileContent>
+                            <ProfileContent>
+                                작성한 댓글 수: {test.myCommentList.length}개
+                            </ProfileContent>
+                            <ProfileContent>
+                                좋아요 누른 수 : {test.myLikeList.length}개
+                            </ProfileContent>
+                        </HoverContent>
+                    </HoverContainer>
                 </WriterContainer>
                 <ContentContainer>{props.comment.content}</ContentContainer>
             </BodyContainer>
         </SingleCommentContainer>
     )
 }
+
 export default SingleComment
