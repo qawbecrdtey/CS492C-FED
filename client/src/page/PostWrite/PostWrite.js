b30b680bf367e5a40a3d5a9c1ce66ea8496a0ca1
amy279 <amy279@kaist.ac.kr>
2021-12-02 22:28:48 +0900

    post write fix

--- client/src/page/PostWrite/PostWrite.js
+++ client/src/page/PostWrite/PostWrite.js
@@ -11,6 +11,7 @@ import { useHistory } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 import { registerPost, updatePostNum, getAllPost } from '../../actions/actions';
 import MDEditor from '@uiw/react-md-editor';
+import axios from 'axios';
 const POST_URL = '/api/post';
 
 const converter = new Showdown.Converter({
@@ -31,34 +32,67 @@ const PostWrite = () => {
         setTitle(e.target.value);
     }
 
-    async function saveContent () {
-        const _data = await request('get', POST_URL + '/currentposts', null);
-        const rcv_data = Object.values(_data);
-        console.log(rcv_data[0]);
-        const created_date = new Date();
-        let body = {
-            postNO: rcv_data[0] + 1,
-            title: title,
-            no_comments: 0,
-            likes: 0,
-            userID: _loginUser['userID'],
-            created_date: created_date,
-            views: 0,
-            content: content,
-        }
-        // dispatch(registerPost(body));
-        if (body['userID'] == '') {
-            console.log("userID is none");
-            return false;
-        }
-        await request('post', POST_URL + '/register', body);
-        getAllPost();
-        let PostNumBody = {
-            num_of_total_posts: rcv_data[0]+1,
-            current_top_post_num: rcv_data[0]+1,
-        }
-        console.log(dispatch(updatePostNum(PostNumBody)));
-        history.push('/postMain/1');
+    function pre_saveContent () {
+        // const _data = request('get', POST_URL + '/currentposts', null);
+        axios.get(POST_URL + '/currentposts', null)
+            .then(_data => {
+                const rcv_data = Object.values(_data);
+                console.log(rcv_data[0].num_of_total_posts);
+                const created_date = new Date();
+                let body = {
+                    postNO: rcv_data[0].num_of_total_posts + 1,
+                    title: title,
+                    no_comments: 0,
+                    likes: 0,
+                    userID: _loginUser['userID'],
+                    created_date: created_date,
+                    views: 0,
+                    content: content,
+                }
+                // dispatch(registerPost(body));
+                if (body['userID'] == '') {
+                    console.log("userID is none");
+                    return false;
+                }
+                request('post', POST_URL + '/register', body);
+                getAllPost();
+                console.log('1');
+                history.push('/postMain/1');
+            })
+        // const rcv_data = Object.values(_data);
+        // console.log(rcv_data[0]);
+        // const created_date = new Date();
+        // let body = {
+        //     postNO: rcv_data[0] + 1,
+        //     title: title,
+        //     no_comments: 0,
+        //     likes: 0,
+        //     userID: _loginUser['userID'],
+        //     created_date: created_date,
+        //     views: 0,
+        //     content: content,
+        // }
+        // // dispatch(registerPost(body));
+        // if (body['userID'] == '') {
+        //     console.log("userID is none");
+        //     return false;
+        // }
+        // request('post', POST_URL + '/register', body);
+        // getAllPost();
+        // console.log('1');
+
+
+        // let PostNumBody = {
+        //     num_of_total_posts: rcv_data[0]+1,
+        //     current_top_post_num: rcv_data[0]+1,
+        // }
+        // console.log(dispatch(updatePostNum(PostNumBody)));
+        // history.push('/postMain/1');
+    }
+
+    const saveContent = () => {
+        pre_saveContent();
+        // history.push('/postMain/1');
     }
     const toPostList = () => {
         history.push('/postMain/1');
