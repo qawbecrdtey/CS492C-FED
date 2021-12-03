1c17943399728e5b422c94ff379d55e9e34402ff
yuminyumin <jungym887@naver.com>
2021-12-04 01:47:47 +0900

    chage max length of ID

--- server/models/User.js
+++ server/models/User.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = mongoose.Schema({
   userID: {
     type: String,
-    maxlength: 20,
+    maxlength: 15,
     required: true,
   },
   password: {
