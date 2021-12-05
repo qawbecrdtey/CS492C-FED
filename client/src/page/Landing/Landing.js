fbba80d278a64c4af14a697e079b19d859bafb30
amy279 <amy279@kaist.ac.kr>
2021-12-06 01:20:42 +0900

    minor fix

--- client/src/page/Landing/Landing.js
+++ client/src/page/Landing/Landing.js
@@ -3,15 +3,6 @@ import { Container} from './styled';
 import Login from '../../component/Login'
 
 const Landing = () => {//Functional Component 만들기
-
-    // const [data, setData] = useState('')
-
-    // useEffect(() => {
-    //     axios.get('api/hello')      //endpoint. getRequest를 server 즉 index.js로 보내질 것
-    //     .then(response => setData(response.data))   //server 에서 돌아온 response를 콘솔창에 출력해봄
-    // }, [])
-    
-    
     return(
          <Container>
           <Login />
