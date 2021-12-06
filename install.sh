46d00c3be9c428abf634c58df575eda14987daba
qawbecrdtey <qawbecrdtey@naver.com>
2021-12-06 18:28:36 +0900

    Added message to shell scripts

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
+echo "Running install.sh..."
+
 cd server
 npm install
 cd ../client
 npm install
 cd ..
+
+echo "Finished install.sh."
--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
+echo "Running run.sh..."
+
 (bash run-client.sh) & (bash run-server.sh) && fg
